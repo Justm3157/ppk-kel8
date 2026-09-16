a72473c05240a418991e6d364bbe3ae3663e1b91
Justm3157 <johanreinhartcalvin@gmail.com>
2026-09-16 20:05:29 +0700

    Fitur tugas tambahan

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\ListController;
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('lists', ListController::class);
+});
